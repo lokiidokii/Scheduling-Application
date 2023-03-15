643603c9148842c603262b9cae40fbca88618a2f
LabUser <LabUser@DESKTOP-BT4131D>
2023-03-15 10:32:52 -0600

    updated alerts

--- src/model/Alerts.java
+++ src/model/Alerts.java
@@ -47,171 +47,171 @@ public class Alerts {
 
         switch (alertType) {
             case 1:
-                alert.setTitle("Error");
-                alert.setHeaderText("All Fields Required");
-                alert.setContentText("Name field is empty.");
+                alert.setTitle("FIELD EMPTY  |  NAME REQUIRED");
+                alert.setHeaderText("Name field is empty");
+                alert.setContentText("Please enter name");
                 alert.showAndWait();
                 break;
             case 2:
-                alert.setTitle("Error");
-                alert.setHeaderText("All Fields Required");
-                alert.setContentText("Address field is empty.");
+                alert.setTitle("FIELD EMPTY  |  ADDRESS REQUIRED");
+                alert.setHeaderText("Address field is empty");
+                alert.setContentText("Please enter valid address");
                 alert.showAndWait();
                 break;
             case 3:
-                alert.setTitle("Error");
-                alert.setHeaderText("All Fields Required");
-                alert.setContentText("City must be selected.");
+                alert.setTitle("FIELD EMPTY  |  STATE/PROVINCE REQUIRED");
+                alert.setHeaderText("No state/province selected");
+                alert.setContentText("Please select your state/province from teh dropdown");
                 alert.showAndWait();
                 break;
             case 4:
-                alert.setTitle("Error");
-                alert.setHeaderText("All Fields Required");
-                alert.setContentText("Postal code field is empty.");
+                alert.setTitle("FIELD EMPTY  |  POSTAL CODE REQUIRED");
+                alert.setHeaderText("Postal code field is empty");
+                alert.setContentText("Please enter postal code");
                 alert.showAndWait();
                 break;
             case 5:
-                alert.setTitle("Error");
-                alert.setHeaderText("All Fields Required");
-                alert.setContentText("Phone number field is empty.");
+                alert.setTitle("FIELD EMPTY  |  PHONE NUMBER REQUIRED");
+                alert.setHeaderText("Phone number field is empty");
+                alert.setContentText("Please enter phone number with dashes, ex: 123-456-7890");
                 alert.showAndWait();
                 break;
             case 6:
-                alertForSave.setTitle("Saved Customer");
-                alertForSave.setHeaderText("Customer Saved to the Database");
-                alertForSave.setContentText("New customer has been saved");
+                alertForSave.setTitle("SAVE SUCCESSFUL |  NEW CUSTOMER ADDED");
+                alertForSave.setHeaderText("Customer information has been saved to the database");
+                alertForSave.setContentText("New customer has been added");
                 alertForSave.showAndWait();
                 break;
             case 7:
-                alertForSave.setTitle("Modified Customer");
-                alertForSave.setHeaderText("Customer Modified ");
-                alertForSave.setContentText("Customer has been modified and saved");
+                alertForSave.setTitle("SAVE SUCCESSFUL  |  CUSTOMER MODIFIED");
+                alertForSave.setHeaderText("Customer information has been saved to the database");
+                alertForSave.setContentText("Existing customer has been modified");
                 alertForSave.showAndWait();
                 break;
             case 8:
-                alertForSave.setTitle("Deleted Customer");
-                alertForSave.setHeaderText("Are you sure you want to delete this customer?");
-                alertForSave.setContentText("Deleting the customer will remove them and their appointments");
+                alertForSave.setTitle("ARE YOU SURE  || DELETE CUSTOMER");
+                alertForSave.setHeaderText("Delete this customer?");
+                alertForSave.setContentText("This is action is permanent and will delete the customer's saved information and any associated appointments you have with them");
                 alertForSave.showAndWait();
                 break;
             case 9:
-                alert.setTitle("No Highlighted Customer");
-                alert.setHeaderText("Customer was not highlighted");
-                alert.setContentText("Please highlight a customer");
+                alert.setTitle("PLEASE SELECT A CUSTOMER");
+                alert.setHeaderText("Please select a customer");
+                alert.setContentText("Highlight the customer you'd like to modify/delete by clicking on the row they're associated with");
                 alert.showAndWait();
                 break;
             case 10:
-                alert.setTitle("No Country Selected");
-                alert.setHeaderText("Country field cannot me empty");
-                alert.setContentText("Please select a country");
+                alert.setTitle("FIELD EMPTY  |  COUNTRY REQUIRED");
+                alert.setHeaderText("No Country Selected");
+                alert.setContentText("Please select a country from the dropdown");
                 alert.showAndWait();
                 break;
             case 11:
-                alert.setTitle("Customer Deleted");
-                alert.setHeaderText("Customer was deleted from the the database successfully.");
-                alert.setContentText("All customer information and appointments were deleted.");
+                alert.setTitle("CUSTOMER SUCCESSFULLY DELETED");
+                alert.setHeaderText("The customer has been deleted from the database");
+                alert.setContentText("All customer information and appointments have been permanently removed");
                 alert.showAndWait();
                 break;
             case 12:
-                alert.setTitle("User Id Not Found");
-                alert.setHeaderText("Couldn't find User_Id from that username");
+                alert.setTitle("ERROR  |  USER ID NOT FOUND");
+                alert.setHeaderText("Could not find User_Id from that username");
                 alert.setContentText("Please enter a valid username");
                 alert.showAndWait();
                 break;
             case 13:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Title field is empty.");
+                alert.setContentText("Title field is empty");
                 alert.showAndWait();
                 break;
             case 14:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Description field is empty.");
+                alert.setContentText("Description field is empty");
                 alert.showAndWait();
                 break;
             case 15:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Location field is empty.");
+                alert.setContentText("Location field is empty");
                 alert.showAndWait();
                 break;
             case 16:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Type field is empty.");
+                alert.setContentText("Type field is empty");
                 alert.showAndWait();
                 break;
             case 17:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Date picker is empty.");
+                alert.setContentText("Date picker is empty");
                 alert.showAndWait();
                 break;
             case 18:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Start time field is empty.");
+                alert.setContentText("Start time field is empty");
                 alert.showAndWait();
                 break;
             case 19:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("End time field is empty.");
+                alert.setContentText("End time field is empty");
                 alert.showAndWait();
                 break;
             case 20:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Existing customer field is empty.");
+                alert.setContentText("Existing customer field is empty");
                 alert.showAndWait();
                 break;
             case 21:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("User ID field is empty.");
+                alert.setContentText("User ID field is empty");
                 alert.showAndWait();
                 break;
             case 22:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Contact is empty.");
+                alert.setContentText("Contact is empty");
                 alert.showAndWait();
                 break;
             case 23:
-                alertForSave.setTitle("Saved Appointment");
-                alertForSave.setHeaderText("Appointment Saved to the Database");
-                alertForSave.setContentText("New Appointment has been saved");
+                alertForSave.setTitle("APPOINTMENT SUCCESSFULLY SAVED");
+                alertForSave.setHeaderText("New appointment added");
+                alertForSave.setContentText("Your appointment has been saved to the Database");
                 alertForSave.showAndWait();
                 break;
             case 24:
-                alert.setTitle("Error");
-                alert.setHeaderText("Appointment Cannot End Before It Starts");
-                alert.setContentText("End time must be after start time.");
+                alert.setTitle("ERROR  |  TIME CONFLICT");
+                alert.setHeaderText("Appointment end time cannot be before start time");
+                alert.setContentText("Please select a new time for your appointment");
                 alert.showAndWait();
                 break;
             case 25:
-                alert.setTitle("Error");
-                alert.setHeaderText("Appointment Cannot Start At the Same Time It Ends.");
-                alert.setContentText("Please select a end time that is after the start time.");
+                alert.setTitle("ERROR  |  TIME CONFLICT");
+                alert.setHeaderText("Appointment start time cannot be at the same time it ends");
+                alert.setContentText("Please select a new time for your appointment");
                 alert.showAndWait();
                 break;
             case 26:
-                alert.setTitle("Error");
-                alert.setHeaderText("Appointment Collision");
-                alert.setContentText("A customer already has an appointment with this time. Please select another time.");
+                alert.setTitle("ERROR  |  TIME CONFLICT");
+                alert.setHeaderText("Appointment overlaps with an existing appointment");
+                alert.setContentText("Please select a new time for your appointment");
                 alert.showAndWait();
                 break;
             case 28:
-                alert.setTitle("Error");
+                alert.setTitle("ERROR  |  FIELD EMPTY");
                 alert.setHeaderText("All Fields Required");
-                alert.setContentText("Fields cannot be empty");
+                alert.setContentText("Please ensure all fields are filled");
                 alert.showAndWait();
                 break;
             case 29:
-                alert.setTitle("Error");
-                alert.setHeaderText("Appointment Outside of Business Hours");
-                alert.setContentText("Appointment must be within business hours.");
+                alert.setTitle("ERROR  |  TIME CONFLICT");
+                alert.setHeaderText("Appointment is outside of operating hours");
+                alert.setContentText("Please select a new time for your appointment");
                 alert.showAndWait();
                 break;
         }
